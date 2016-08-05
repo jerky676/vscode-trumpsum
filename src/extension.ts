f58ae7ace2362eb053556994c846b8652d35270b
Justin <jluther@intesolv.com>
2016-08-05 17:06:12 -0500

    trump refactor

--- src/extension.ts
+++ src/extension.ts
@@ -10,36 +10,38 @@ export function activate(context: vscode.ExtensionContext) {
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "hipsum" is now active!');
+    console.log('Congratulations, your extension "trumpsum" is now active!');
 
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.hipsum', () => {
+    let disposable = vscode.commands.registerCommand('extension.trumpsum', () => {
         // The code you place here will be executed every time your command is executed
 
         const editor = vscode.window.activeTextEditor;
-
-        http.get('http://hipsterjesus.com/api/?paras=1&html=false', (res) => {
-            var body = '';
+        var cheerio = require('cheerio')
+        var data = "";
+       http.get('http://trumpipsum.net/?paras=1&type=make-it-great', (res) => {
             res.on('data', function (chunk) {
-                body += chunk;
+                data += chunk;
             });
             res.on('end', function () {
-                console.log(body);
-                editor.edit((eb) => {
-                    eb.insert(editor.selection.active, JSON.parse(body).text);
-                });
+                var html = cheerio.load(data);
+                var text = String(html('div.anyipsum-output > p')).replace('<p> ','').replace('</p>','');
+                console.log(text);
+
             });
             res.on('error', function (e) {
                 console.log("Got error: " + e.message);
             });
+        });
     });
-});
 
-context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {
 }
+
+
